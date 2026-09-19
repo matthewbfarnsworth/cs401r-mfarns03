1dc886fcea83e07d41f6e409d45a9aed4798c946
Matthew Farnsworth <matthewbfarnsworth@gmail.com>
2026-09-19 17:59:43 -0600

    feat: add terraform modules (Task B1)

--- infrastructure/modules/iam/main.tf
+++ infrastructure/modules/iam/main.tf
@@ -9,4 +9,93 @@
 # prefixes and SageMaker actions this role actually needs. A wildcard policy
 # here will cost you points in Lab 2.
 
-# TODO: implement the three resources above.
+data "aws_iam_policy_document" "sagemaker_trust" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["sagemaker.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ml_engineer" {
+  name               = "${var.project}-${var.environment}-MLEngineer"
+  assume_role_policy = data.aws_iam_policy_document.sagemaker_trust.json
+
+  tags = {
+    Name = "${var.project}-${var.environment}-MLEngineer"
+  }
+}
+
+resource "aws_iam_policy" "ml_engineer" {
+  name        = "${var.project}-${var.environment}-NorthStarMLEngineerPolicy"
+  description = "Policy for MLEngineer role"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "SageMakerCore",
+        "Effect" : "Allow",
+        "Action" : [
+          "sagemaker:CreateTrainingJob", "sagemaker:DescribeTrainingJob", "sagemaker:StopTrainingJob",
+          "sagemaker:CreateEndpoint", "sagemaker:DescribeEndpoint", "sagemaker:DeleteEndpoint",
+          "sagemaker:CreateEndpointConfig", "sagemaker:DeleteEndpointConfig",
+          "sagemaker:CreateMlflowApp", "sagemaker:DescribeMlflowApp", "sagemaker:ListMlflowApps",
+          "sagemaker:CreatePresignedMlflowAppUrl",
+          "sagemaker:RegisterModel", "sagemaker:DescribeModelPackage", "sagemaker:ListModelPackages"
+        ],
+        "Resource" : "*"
+      },
+      {
+        "Sid" : "StudioSelfService",
+        "Effect" : "Allow",
+        "Action" : [
+          "sagemaker:DescribeDomain", "sagemaker:ListDomains",
+          "sagemaker:DescribeUserProfile", "sagemaker:ListUserProfiles",
+          "sagemaker:DescribeSpace", "sagemaker:ListSpaces", "sagemaker:CreateSpace",
+          "sagemaker:UpdateSpace", "sagemaker:DeleteSpace",
+          "sagemaker:DescribeApp", "sagemaker:ListApps", "sagemaker:CreateApp", "sagemaker:DeleteApp",
+          "sagemaker:CreatePresignedDomainUrl"
+        ],
+        "Resource" : [
+          "arn:aws:sagemaker:*:*:domain/*", "arn:aws:sagemaker:*:*:user-profile/*",
+          "arn:aws:sagemaker:*:*:space/*", "arn:aws:sagemaker:*:*:app/*"
+        ]
+      },
+      {
+        "Sid" : "S3ArtifactsAndFeatures",
+        "Effect" : "Allow",
+        "Action" : ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
+        "Resource" : [
+          "arn:aws:s3:::${var.project}-${var.environment}-data-*/artifacts/*",
+          "arn:aws:s3:::${var.project}-${var.environment}-data-*/features/*"
+        ]
+      },
+      {
+        "Sid" : "S3BucketList",
+        "Effect" : "Allow",
+        "Action" : ["s3:ListBucket", "s3:GetBucketLocation"],
+        "Resource" : "arn:aws:s3:::${var.project}-${var.environment}-data-*"
+      },
+      {
+        "Sid" : "CloudWatchLogs",
+        "Effect" : "Allow",
+        "Action" : ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"],
+        "Resource" : "arn:aws:logs:*:*:log-group:/aws/sagemaker/*"
+      },
+      {
+        "Sid" : "ECRRead",
+        "Effect" : "Allow",
+        "Action" : ["ecr:GetDownloadUrlForLayer", "ecr:BatchGetImage", "ecr:GetAuthorizationToken"],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ml_engineer" {
+  role       = aws_iam_role.ml_engineer.name
+  policy_arn = aws_iam_policy.ml_engineer.arn
+}
--- infrastructure/modules/sagemaker/main.tf
+++ infrastructure/modules/sagemaker/main.tf
@@ -17,4 +17,35 @@
 # and the security group from modules/vpc; both arrive as variables. See the
 # module call in environments/dev/main.tf.
 
-# TODO: implement the two resources above.
+resource "aws_sagemaker_domain" "this" {
+  domain_name = "${var.project}-${var.environment}-domain"
+  auth_mode   = "IAM"
+  vpc_id      = var.vpc_id
+  subnet_ids  = var.subnet_ids
+
+  default_user_settings {
+    execution_role  = var.execution_role_arn
+    security_groups = var.security_group_ids
+  }
+
+  retention_policy {
+    home_efs_file_system = "Delete"
+  }
+
+  tags = {
+    Name = "${var.project}-${var.environment}-domain"
+  }
+}
+
+resource "aws_sagemaker_user_profile" "ml_engineer" {
+  domain_id         = aws_sagemaker_domain.this.id
+  user_profile_name = "MLEngineer"
+
+  user_settings {
+    execution_role = var.execution_role_arn
+  }
+
+  tags = {
+    Name = "${var.project}-${var.environment}-MLEngineer"
+  }
+}
